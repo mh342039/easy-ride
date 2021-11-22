ace645421a8f16e6d1b256dbc4e4ff8821d69704
Mohammed Hamza Jasnak <mh342039@dal.ca>
2021-11-21 21:33:24 -0400

    Added basic routing for the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { mainModule } from 'process';
+import { HomepageComponent } from './component/homepage/homepage.component';
+import { MainComponent } from './component/main/main.component';
+import { MyRidesComponent } from './component/my-rides/my-rides.component';
+import { PublishRideComponent } from './component/publish-ride/publish-ride.component';
+import { RideDetailsComponent } from './component/ride-details/ride-details.component';
+import { SearchResultComponent } from './component/search-result/search-result.component';
+import { SignInComponent } from './component/sign-in/sign-in.component';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
+import { UpdatePasswordComponent } from './component/update-password/update-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'updatePassword', component: UpdatePasswordComponent },
+  {
+    path: 'main', component: MainComponent, children: [
+      { path: '', redirectTo: 'home-page', pathMatch: 'full' },
+      { path: 'home-page', component: HomepageComponent },
+      { path: 'search-result', component: SearchResultComponent },
+      { path: 'ride-details', component: RideDetailsComponent },
+      { path: 'my-rides', component: MyRidesComponent },
+      { path: 'publish-ride', component: PublishRideComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
