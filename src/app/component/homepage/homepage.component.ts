f0f8791b82fe660c214c003f4eb86343ce287ce4
preethijeeva1 <pr316875@dal.ca>
2021-11-29 20:56:58 -0400

    added terms and conditions, faqs and contact us page

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import {MatDialog} from '@angular/material/dialog';
+import { TermsComponent } from '../terms/terms.component';
+import {FaqComponent} from '../faq/faq.component';
+import {ContactComponent} from '../contact/contact.component'
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog, private router: Router, public _dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
+  termsnc(){
+ 
+      const dialogRef = this.dialog.open(TermsComponent,{
+        width: '500px',
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        
+      });
+  }
+  faqs(){
+ 
+    const dialogRef = this.dialog.open(FaqComponent,{
+      width: '500px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
+    });
+}
+
+contact(){
+ 
+  const dialogRef = this.dialog.open(ContactComponent,{
+    width: '500px',
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    
+  });
+}
 }
